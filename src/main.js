1e9c8e97384b0f39051caa2008be3136b09e68b7
Yassine El Yacoubi <43663221+awsm36z@users.noreply.github.com>
2023-09-25 18:10:00 -0700

    improved code to handle null time errors

--- src/main.js
+++ src/main.js
@@ -4,66 +4,82 @@ const fs = require("fs");
 let year = new Date().getFullYear();
 let day = new Date().getDate();
 let month = new Date().getMonth();
-const fileUrl = `./IMG_3219.jpg`;
+const fileUrl = `./IMG_3214.jpg`;
 const positions = {
   TBHExit: "Butterfly House Exit",
   TBHIn: "Butterfly House Enterance",
   Tidepool: "Tidepool",
 };
-  let times = [
-    "10:00-11:00",
-    "11:00-12:00",
-    "12:00-13:00",
-    "13:00-14:00",
-    "14:00-15:00",
-    "15:00-16:00",
-    "16:00-17:00",
-    "17:00-17:15",
-  ];
+let times = [
+  "10:00-11:00",
+  "11:00-12:00",
+  "12:00-13:00",
+  "13:00-14:00",
+  "14:00-15:00",
+  "15:00-16:00",
+  "16:00-17:00",
+  "17:00-17:15",
+];
 let finalSchedule = [];
 
 function getScheduleData() {
-fetch("http://localhost:3000", {
-  method: "POST",
-  headers: {
-    "Content-Type": "application/json",
-  },
-  body: JSON.stringify({ fileUrl: fileUrl }),
-})
-  .then((response) => response.json())
-  .then((data) => {
-    clean = dataClean(data.schedule);
-    finalSchedule = defSchedule(clean);
-
-    const events = finalSchedule.map((item) => {
-      const title = Object.keys(item)[0];
-      const time = item[title];
-      console.log(parseInt(time.split("-")[0].split(":")[0]))
-      console.log(parseInt(time.split("-")[0].split(":")[1]))
-      console.log(parseInt(time.split("-")[1].split(":")[0]))
-      console.log(parseInt(time.split("-")[1].split(":")[1]))
-
-      return {
-        start: [year, month, day, /**hour */ parseInt(time.split("-")[0].split(":")[0]), parseInt(time.split("-")[0].split(":")[1]) ], // Assuming time format is "HH:mm-HH:mm"
-        end: [year, month, day, /**hour */ parseInt(time.split("-")[1].split(":")[0]), parseInt(time.split("-")[1].split(":")[1]) ], // Splitting start and end times
-        title: title,
-      };
-    });
-
-    console.log(JSON.stringify(events));
-    //console.log(`FINAL SCHEDULE: ${JSON.stringify(finalSchedule, null, 1)}`);
-    const { error, value } = ics.createEvents(events);
-
-    if (error) {
-      console.error("Error:", error);
-    } else {
-      fs.writeFileSync("schedule.ics", value);
-      console.log("ICS file written successfully!");
-    }
+  fetch("http://localhost:3000", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ fileUrl: fileUrl }),
   })
-  .catch((error) => {
-    console.error("Error:", error);
-  });
+    .then((response) => response.json())
+    .then((data) => {
+      clean = dataClean(data.schedule);
+      finalSchedule = defSchedule(clean);
+
+      const events = finalSchedule.map((item) => {
+        const title = Object.keys(item)[0];
+        const time = item[title];
+        if (time == undefined) {
+          return null;
+          // {
+          //  start: [year, month, day, 0, 0], // Assuming time format is "HH:mm-HH:mm"
+          //  end: [year, month, day, 0, 1], // Splitting start and end times
+          //  title: "Bug",
+          //};
+        }
+        //console.log(time)
+        return {
+          start: [
+            year,
+            month,
+            day,
+            /**hour */ parseInt(time.split("-")[0].split(":")[0]),
+            parseInt(time.split("-")[0].split(":")[1]),
+          ], // Assuming time format is "HH:mm-HH:mm"
+          end: [
+            year,
+            month,
+            day,
+            /**hour */ parseInt(time.split("-")[1].split(":")[0]),
+            parseInt(time.split("-")[1].split(":")[1]),
+          ], // Splitting start and end times
+          title: title,
+        };
+      }).filter(event => event !== null); // Filter out null values;
+
+      console.log(JSON.stringify(events));
+      //console.log(`FINAL SCHEDULE: ${JSON.stringify(finalSchedule, null, 1)}`);
+      const { error, value } = ics.createEvents(events);
+
+      if (error) {
+        console.error("Error:", error);
+      } else {
+        fs.writeFileSync("schedule.ics", value);
+        console.log("ICS file written successfully!");
+      }
+    })
+    .catch((error) => {
+      console.error("Error:", error);
+    });
 }
 
 function dataClean(schedule) {
@@ -111,6 +127,5 @@ function defSchedule(schedule) {
   }
   return newSchedule;
 }
-
 
 getScheduleData();
